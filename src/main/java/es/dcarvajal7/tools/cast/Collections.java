e6b306a554d1c1871ee017faba90f1f4d68a52c5
dCarvajal7 <danielcarvajalpelicano@gmail.com>
2022-07-09 00:59:55 +0200

    collections generic transform

---
+++ src/main/java/es/dcarvajal7/tools/cast/Collections.java
@@ -0,0 +1,19 @@
+package es.dcarvajal7.tools.cast;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Collections {
+
+    /**
+     * Generic method to convert list of T to list of T
+     */
+    public static <T, U> List<U> transform(List<T> list, Function<T, U> function) {
+        return list.stream()
+                .map(function)
+                .collect(Collectors.toList());
+    }
+
+
+}
