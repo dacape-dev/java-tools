534fa7daf76ea3beb7f714995d2cb747fd9a6f15
dCarvajal7 <danielcarvajalpelicano@gmail.com>
2022-08-13 22:36:02 +0200

    uuid validator

---
+++ src/main/java/es/dcarvajal7/tools/UuidValidator.java
@@ -0,0 +1,17 @@
+package es.dcarvajal7.tools;
+
+import java.util.regex.Pattern;
+
+public class UuidValidator {
+
+    private final static Pattern UUID_REGEX_PATTERN =
+            Pattern.compile("^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$");
+
+    public static boolean isValidUUID(String str) {
+        if (str == null) {
+            return Boolean.FALSE;
+        }
+        return UUID_REGEX_PATTERN.matcher(str).matches();
+    }
+
+}
---
+++ src/test/java/es/dcarvajal7/tools/UuidValidatorUnitTest.java
@@ -0,0 +1,41 @@
+package es.dcarvajal7.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UuidValidatorUnitTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void whenUuid_thenReturnIsValidUuid() {
+        assertThat(UuidValidator.isValidUUID("009692ee-f930-4a74-bbd0-63b8baa5a927")).isTrue();
+    }
+
+    @Test
+    public void whenUuid_thenReturnIsValidUuidKO1() {
+        assertThat(UuidValidator.isValidUUID(null)).isFalse();
+    }
+
+    @Test
+    public void whenUuid_thenReturnIsValidUuidKO2() {
+        assertThat(UuidValidator.isValidUUID("")).isFalse();
+    }
+
+    @Test
+    public void whenUuid_thenReturnIsValidUuidKO3() {
+        assertThat(UuidValidator.isValidUUID("test-ss-ss-ss-s")).isFalse();
+    }
+
+    @Test
+    public void whenUuid_thenReturnIsValidUuidKO4() {
+        assertThat(UuidValidator.isValidUUID("009692ee-f9309-4a74-bbd0-63b8baa5a927")).isFalse();
+    }
+
+
+}
